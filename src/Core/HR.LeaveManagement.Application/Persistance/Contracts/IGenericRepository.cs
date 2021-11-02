40ebb30976faae8db8ec08a19d3e83138df6299b
deyan <deyan.todorov@hotmail.com>
2021-11-02 23:09:54 +0200

    * HR.LeaveManagement.sln: add db layer
    * IGenericRepository.cs:
    * ILeaveTypeRepository.cs:

--- src/Core/HR.LeaveManagement.Application/Persistance/Contracts/IGenericRepository.cs
+++ src/Core/HR.LeaveManagement.Application/Persistance/Contracts/IGenericRepository.cs
@@ -13,8 +13,8 @@ namespace HR.LeaveManagement.Application.Persistance.Contracts
 
         public Task<T> Add(T entity);
 
-        public Task<T> Update(T entity);
+        public Task Update(T entity);
 
-        public Task<T> Delete(T entity);
+        public Task Delete(T entity);
     }
 }
--- src/Core/HR.LeaveManagement.Application/Persistance/Contracts/ILeaveTypeRepository.cs
+++ src/Core/HR.LeaveManagement.Application/Persistance/Contracts/ILeaveTypeRepository.cs
@@ -1,14 +1,8 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-
-using HR.LeaveManagement.Domain;
+﻿using HR.LeaveManagement.Domain;
 
 namespace HR.LeaveManagement.Application.Persistance.Contracts
 {
     public interface ILeaveTypeRepository : IGenericRepository<LeaveType>
     {
-        Task<LeaveRequest> GetLeaveRequestWithDetails(int id);
-
-        Task<List<LeaveRequest>> GetLeaveRequestsWithDetails();
     }
 }
