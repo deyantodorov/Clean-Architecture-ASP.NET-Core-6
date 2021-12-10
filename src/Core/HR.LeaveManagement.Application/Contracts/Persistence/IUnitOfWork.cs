a041cb96dcdd4c157f77d06f5e00badbab8da4a1
Deyan Todorov <deyan.todorov@endava.com>
2021-12-10 05:59:18 +0200

    uow part 2

---
+++ src/Core/HR.LeaveManagement.Application/Contracts/Persistence/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Contracts.Persistence
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ILeaveAllocationRepository LeaveAllocationRepository { get; }
+
+        ILeaveRequestRepository LeaveRequestRepository { get; }
+
+        ILeaveTypeRepository LeaveTypeRepository { get; }
+
+        Task Save();
+    }
+}
